84ac8447bc34f06eb7b6fdf7e2e41485125870ce
LoremIPSummer <goodiesoftofficial@gmail.com>
2022-12-19 14:43:06 +0100

    Add prettier configuration

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,69 +1,71 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 import {
-  CdkDragDrop,
-  moveItemInArray,
-  transferArrayItem,
-} from '@angular/cdk/drag-drop';
-import { Board } from 'src/app/models/board.model';
-import { Column } from 'src/app/models/column.modul';
-import { Todo } from 'src/app/models/todo.modul';
+    CdkDragDrop,
+    moveItemInArray,
+    transferArrayItem,
+} from '@angular/cdk/drag-drop'
+import { Board } from 'src/app/models/board.model'
+import { Column } from 'src/app/models/column.modul'
+import { Todo } from 'src/app/models/todo.modul'
 @Component({
-  selector: 'app-main-view',
-  templateUrl: './main-view.component.html',
-  styleUrls: ['./main-view.component.scss'],
+    selector: 'app-main-view',
+    templateUrl: './main-view.component.html',
+    styleUrls: ['./main-view.component.scss'],
 })
 export class MainViewComponent {
-  board: Board = new Board('Test Board', [
-    new Column('TODO', []),
-    new Column('DOING', []),
-    new Column('VERIFY', []),
-    new Column('DONE', []),
-  ]);
+    board: Board = new Board('Test Board', [
+        new Column('TODO', []),
+        new Column('DOING', []),
+        new Column('VERIFY', []),
+        new Column('DONE', []),
+    ])
 
-  newTodo: string;
-  saveTodo() {
-    if (this.newTodo) {
-      let todo = new Todo();
+    newTodo: string
+    saveTodo() {
+        if (this.newTodo) {
+            let todo = new Todo()
 
-      todo.name = this.newTodo;
-      const searchColumn = this.board.columns.filter(
-        (col) => col.name === 'TODO'
-      );
+            todo.name = this.newTodo
+            const searchColumn = this.board.columns.filter(
+                (col) => col.name === 'TODO'
+            )
 
-      if (searchColumn[0].tasks.find((task) => task.name === this.newTodo))
-        return alert('Todo already exists!');
-      searchColumn[0].tasks.push(todo);
+            if (
+                searchColumn[0].tasks.find((task) => task.name === this.newTodo)
+            )
+                return alert('Todo already exists!')
+            searchColumn[0].tasks.push(todo)
 
-      this.newTodo = '';
-    } else {
-      alert('Please enter Todo!');
+            this.newTodo = ''
+        } else {
+            alert('Please enter Todo!')
+        }
     }
-  }
 
-  rmTodo(todo: Todo, column: Column) {
-    const searchColumn = this.board.columns.filter(
-      (col) => col.name === column.name
-    );
-    searchColumn[0].setColumnTasks(
-      searchColumn[0].tasks.filter((t) => t.name !== todo.name)
-    );
-  }
-
-  drop(event: CdkDragDrop<Todo[]>) {
-    console.log('Container elements: ' + event.container.data);
-    if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
+    rmTodo(todo: Todo, column: Column) {
+        const searchColumn = this.board.columns.filter(
+            (col) => col.name === column.name
+        )
+        searchColumn[0].setColumnTasks(
+            searchColumn[0].tasks.filter((t) => t.name !== todo.name)
+        )
     }
-  }
+
+    drop(event: CdkDragDrop<Todo[]>) {
+        console.log('Container elements: ' + event.container.data)
+        if (event.previousContainer === event.container) {
+            moveItemInArray(
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex
+            )
+        } else {
+            transferArrayItem(
+                event.previousContainer.data,
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex
+            )
+        }
+    }
 }
--- src/app/pages/modify-dialog/modify-dialog.component.ts
+++ src/app/pages/modify-dialog/modify-dialog.component.ts
@@ -1,42 +1,46 @@
-import { Component, Inject } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Component, Inject } from '@angular/core'
+import {
+    MatDialog,
+    MatDialogRef,
+    MAT_DIALOG_DATA,
+} from '@angular/material/dialog'
 
 export interface DialogData {
-  animal: string;
-  name: string;
+    animal: string
+    name: string
 }
 
 export class DialogOverviewExample {
-  animal: string;
-  name: string;
+    animal: string
+    name: string
 
-  constructor(public dialog: MatDialog) {}
+    constructor(public dialog: MatDialog) {}
 
-  openDialog(): void {
-    const dialogRef = this.dialog.open(ModifyDialogComponent, {
-      width: '250px',
-      data: { name: this.name, animal: this.animal },
-    });
+    openDialog(): void {
+        const dialogRef = this.dialog.open(ModifyDialogComponent, {
+            width: '250px',
+            data: { name: this.name, animal: this.animal },
+        })
 
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log('The dialog was closed');
-      this.animal = result;
-    });
-  }
+        dialogRef.afterClosed().subscribe((result) => {
+            console.log('The dialog was closed')
+            this.animal = result
+        })
+    }
 }
 
 @Component({
-  selector: 'app-modify-dialog',
-  templateUrl: './modify-dialog.component.html',
-  styleUrls: ['./modify-dialog.component.scss'],
+    selector: 'app-modify-dialog',
+    templateUrl: './modify-dialog.component.html',
+    styleUrls: ['./modify-dialog.component.scss'],
 })
 export class ModifyDialogComponent {
-  constructor(
-    public dialogRef: MatDialogRef<ModifyDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData
-  ) {}
+    constructor(
+        public dialogRef: MatDialogRef<ModifyDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: DialogData
+    ) {}
 
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
+    onNoClick(): void {
+        this.dialogRef.close()
+    }
 }
